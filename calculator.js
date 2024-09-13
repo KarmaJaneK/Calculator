afddfd5308a16d1492afa511ac896bd35a4583e5
Karma Nel <karmajane27@gmail.com>
2024-09-13 06:55:44 +0000

    add functionality to - update firstNumber, secondNumber and Operator based on button selection -call operate function when '=' key is pressed -update display with result

--- calculator.js
+++ calculator.js
@@ -20,10 +20,11 @@ const multiply = function(a,b){
 const divide = function(a,b){
     return a / b;
 };
+ //store the first and second number and the operator
 
-let firstNumber = 0;
-let operator = '';
-let secondNumber = 0;
+ let firstNumber = 0;
+ let operator = '';
+ let secondNumber = 0;
 
 // takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 const operate = function(operator, a, b){
@@ -50,6 +51,25 @@ const operate = function(operator, a, b){
     }
     document.querySelectorAll('button').forEach(button => { //add event listener to all buttons
         button.addEventListener('click',() => {
-            display(button.innerText);
-        });
+            const value = button.innerText;
+
+            if(!NaN(value)){//if the value is a number
+                if (operator === ''){ //if the operator is empty, the value is the first number
+                    firstNumber += value;
+                    display(firstNumber);
+                } else {
+                    secondNumber += value; //if the operator is not empty, the value is the second number
+                    display(secondNumber);
+                }
+            } else if (value === 'C'){ //clear the display
+            } else if (value === '+' || value === '-' || value === '*' || value === '/'){ //if the value is an operator
+                operator = value; 
+            }else if (value === 'Equal'){ //if the value is the equal button
+                display(operate(operator, firstNumber, secondNumber)); //update the display with the result
+            }
+                            
+    });
+
+   
+    
     });
