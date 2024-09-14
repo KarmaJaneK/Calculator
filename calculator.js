a0804f41a62dab9b3402229562e0d861de2722d5
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-09-14 13:57:47 +0700

    add error handling for if - secondNumber and Operator are empty - only SecondNumber is Empty

--- calculator.js
+++ calculator.js
@@ -22,14 +22,14 @@ const divide = function (a, b) {
 };
 //store the first and second number and the operator
 
-let firstNumber = '';
-let operator = '';
-let secondNumber = '';
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
 
 // takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 const operate = function (operator, a, b) {
-    a = Number(a);
-    b = Number(b);//convert the strings to numbers
+  a = Number(a);
+  b = Number(b); //convert the strings to numbers
   switch (operator) {
     case "+":
       return sum(a, b);
@@ -45,11 +45,11 @@ const operate = function (operator, a, b) {
   }
 };
 
-let displayValue = '';
+let displayValue = "";
 
-const display = function(value) {
-    displayValue = value;
-    document.querySelector('.display').innerText = displayValue; // show the value on the display
+const display = function (value) {
+  displayValue = value;
+  document.querySelector(".display").innerText = displayValue; // show the value on the display
 };
 document.querySelectorAll("button").forEach((button) => {
   //add event listener to all buttons
@@ -66,11 +66,9 @@ document.querySelectorAll("button").forEach((button) => {
         secondNumber += value; //if the operator is not empty, the value is the second number
         display(secondNumber);
       }
-
     } else if (value === "C") {
-        //if the value is the clear button
+      //if the value is the clear button
       display(""); //clear the display
-
     } else if (
       value === "+" ||
       value === "-" ||
@@ -81,11 +79,36 @@ document.querySelectorAll("button").forEach((button) => {
       operator = value;
     } else if (value === "=") {
       //if the value is the equal button
-        if (firstNumber === "" || firstNumber === null || firstNumber === undefined) {
-       document.querySelector('.display').innerText = '0'; // show the value on the display
 
+      //if firstNumber is empty, display 0
+      if (
+        firstNumber === "" ||
+        firstNumber === null ||
+        firstNumber === undefined
+      ) {
+        display(0); // show the value on the display
+
+        //if second number and operator is empty, display the first number
+      } else if (
+        (secondNumber === "" ||
+        secondNumber === null ||
+        secondNumber === undefined) 
+        && (operator === "" ||
+        operator === null ||
+        operator === undefined)
+      ) {
+        display(firstNumber);
+
+        //if only second number is empty, concatenate the first number with the operator
+      } else if (
+        secondNumber === "" ||
+        secondNumber === null ||
+        secondNumber === undefined
+      ) {
+        document.querySelector(".display").innerText = firstNumber + operator; 
       } else {
-         display(operate(operator, firstNumber, secondNumber)); //update the display with the result
-    }};
+        display(operate(operator, firstNumber, secondNumber)); //update the display with the result
+      }
+    }
   });
 });
